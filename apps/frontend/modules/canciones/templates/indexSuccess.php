c3449fde0a1198939a3df256ed625fac55d2f6c4
Guestfne <Guestfne@FRCCALL3A8778.empaque.fne>
2012-12-05 03:57:52 -0600

    subiendo crud actual de canciones

--- apps/frontend/modules/canciones/templates/indexSuccess.php
+++ apps/frontend/modules/canciones/templates/indexSuccess.php
@@ -1,7 +1,8 @@
+
 <html>
     
     <head>
-<script language="javascript" type="text/javascript" src="swfobject.js" ></script>  
+  
     </head>
 
 <center><h1>MIS CANCIONES</h1></center>
@@ -9,31 +10,89 @@
 <table border="1" width="65%" cellspacing="0" class="tablesorter">
   <thead>
     <tr>
-      <!--<th>Idcanciones</th>-->
+    
       <th>Nombrecancion</th>
       <th>Interprete</th>
       <th>Album</th>
       <th>Genero</th>
-      <th>Ruta</th>
-      <th>Reproducir</th>
+      <th>Eliminar</th>
+      <th>Editar</th>
+      
       
     </tr>
   </thead>
-  <tbody>
-    <?php foreach ($cancioness as $canciones): ?>
-    <tr>
+  
+<?php
+$reg_por_pagina_can=4;
+@$nro_pagina_can=$_GET['num'];
+if(is_numeric($nro_pagina_can)){
+    $inicio =($nro_pagina_can-1)* $reg_por_pagina_can;
     
-      <td><?php echo $canciones->getNombrecancion() ?></td>
-      <td><?php echo $canciones->getInterprete() ?></td>
-      <td><?php echo $canciones->getAlbum() ?></td>
-      <td><?php echo $canciones->getGenero() ?></td>
-      <td><?php echo $canciones->getRuta() ?></td>
-      <td></td>
-      
-    </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
+}
+ else
+    $inicio=0;
+ $con = Doctrine_Manager::getInstance()->connection();
+$consulta_can="select count(*) from canciones order by idCanciones"; 
 
-  <!--<a href="<?php echo url_for('canciones/new') ?>">New</a>-->
+ 
+$st_can = $con->execute($consulta_can);
+$nro_reg_can = $st_can->fetchColumn();
+
+if($nro_reg_can==0){
+   echo 'no se han encontrado canciones';
+}
+
+ $consultapag_can= "select * from canciones order by idCanciones limit $inicio,$reg_por_pagina_can";
+ $stpag_can=$con->execute($consultapag_can);
+ $rspag_can=$stpag_can->fetchAll();
+ $can_paginas_can=$nro_reg_can / $reg_por_pagina_can;
+
+?>
+    
+  
+  
+       <?php foreach ($rspag_can as $cancionesArray) :
+    $canciones = new Canciones();
+    $canciones->fromArray($cancionesArray);
+    ?>
+  <tbody>
+      
+        <tr>
+             
+            <td><?php echo $cancionesArray['nombreCancion'];?></td>
+            <td><?php echo $cancionesArray['interprete'];?></td>
+            <td><?php echo $cancionesArray['album'];?></td>
+            <td><?php echo $cancionesArray['genero'];?></td>
+               <td><?php echo link_to(image_tag('././././Trash_icon.png', array('alt'=>('Borrar'),'title'=>('Borrar'))),'canciones/delete?idcanciones='.$canciones->getIdCanciones(),array('post' => true, 'confirm' => 'Estas seguro que deseas eliminar?')) ?></td>
+            <td><?php echo link_to(image_tag('././././Edit_icon.png', array('alt'=>('Editar'),'title'=>('Editar'))),'canciones/edit?idcanciones='.$canciones->getIdCanciones())?></td>
+        
+ 
+          </tr>
+            
+
+            </tbody>    
+    <?php endforeach;?>
+            
+  </table>          
+
+<div align="center">
+        <?php
+        if($nro_pagina_can>1)
+           echo " <a href='?num=".($nro_pagina_can - 1)."'> Anterior </a> "." ";
+        
+    for($i=1;$i<=$can_paginas_can;$i++){
+        
+                  if($i ==$nro_pagina_can)
+
+        echo " ".$i." "; 
+         else  
+         echo " " . "<a href='?num=$i'>$i</a>" ." ";
+ }
+ 
+ if($nro_pagina_can<$can_paginas_can)
+           echo" ". "<a href='?num=".($nro_pagina_can + 1)."'>Siguiente</a>";
+     
+  ?>
+</div>
+  
 </html>
