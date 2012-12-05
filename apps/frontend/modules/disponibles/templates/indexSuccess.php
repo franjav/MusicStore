7e73656cefc821979ae3cffc50fc7baefff643db
Jhonny <jmelleo@jmelleo-PC>
2012-12-05 02:01:34 -0500

    Actualizacion Recomendar

--- apps/frontend/modules/disponibles/templates/indexSuccess.php
+++ apps/frontend/modules/disponibles/templates/indexSuccess.php
@@ -16,39 +16,24 @@
     </tr>
   </thead>
   <tbody>
-      
-<?php
-        
-require_once'PHPPaging.lib.php';
-$paging= new PHPPaging;
-$paging->agregarConsulta('select * from disponibles');
-$paging->ejecutar();
 
-
-//$re=mysql_query("select * from disponibles");
-
-
-
-?>
     <?php 
-    while($f= $paging->fetchResultado()){
-    
   foreach ($disponibless as $disponibles): ?>
     <tr>
-      <td><a href="//<?php echo url_for('disponibles/show?iddisponibles='.$disponibles->getIddisponibles()) ?>">ver</a></td>  
-      <td>//<?php echo $disponibles->getNombrecancion() ?></td>
-      <td>//<?php echo $disponibles->getInterprete() ?></td>
-      <td>//<?php echo $disponibles->getAlbum() ?></td>
-      <td>//<?php echo $disponibles->getGenero() ?></td>
-       <td><a href="disponibles/recomend?iddisponibles=//<?php echo $disponibles['iddisponibles'];?>">Recomendar</a></td>     
-      <td>//<?php echo $disponibles->getPrecio() ?></td>
+      <td><a href="<?php echo url_for('disponibles/show?iddisponibles='.$disponibles->getIddisponibles()) ?>">ver</a></td>  
+      <td><?php echo $disponibles->getNombrecancion() ?></td>
+      <td><?php echo $disponibles->getInterprete() ?></td>
+      <td><?php echo $disponibles->getAlbum() ?></td>
+      <td><?php echo $disponibles->getGenero() ?></td>
+       <td><a href="disponibles/recomend?iddisponibles=<?php echo $disponibles['iddisponibles'];?>">Recomendar</a></td>     
+      <td><?php echo $disponibles->getPrecio() ?></td>
           
-      <td><a href="disponibles/add?iddisponibles=//<?php echo $disponibles['iddisponibles'];?>"><img src="../../web/images/carrito.png"></a></td>
+      <td><a href="disponibles/add?iddisponibles=<?php echo $disponibles['iddisponibles'];?>"><img src="../../web/images/carrito.png"></a></td>
      
     </tr>
-    <?php endforeach;} ?>
+    <?php endforeach; ?>
   </tbody>
   
 </table>
-<?php echo'Paginas'.$paging->fetchNavegacion();?>
+
 </html>
